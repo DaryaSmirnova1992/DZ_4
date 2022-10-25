ce1aa1d38eda118d1426e692453821042d2bfff6
Darya <darecc@yandex.ru>
2022-10-25 19:19:01 +0400

    add task1

--- Задача_1.py
+++ Задача_1.py
@@ -1,22 +1,9 @@
 # Вычислить число c заданной точностью d
-# Пример:
-# при d = 0.001, π = 3.141.  10-1 ≤ d ≤10-10
 
-import math
-
-
-d = float(input("точность: "))
-sum = 0
-a = 0
-n = 0
-
-while True:
-    a = ((-1)**n)/(2*n+1)
-    sum += a
-    n += 1
-    pi_new = sum*4
-    if abs(a)*4 < d:
-        break
-
-print(f'Вычисленное значение Пи (формула Лейбница) = {pi_new}')
-print(f'Пи из библиотеки math: {math.pi}')
+import random
+num= random.uniform(1,5)
+print(num)
+degree=random.randint(-10, -1)
+d=10**degree
+result=round(num, -(degree))
+print(f'число {num}, точность округления {d}. Результат: {result}')
